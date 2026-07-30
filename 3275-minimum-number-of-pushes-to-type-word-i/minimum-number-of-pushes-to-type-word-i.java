05731930d229918dafc8df91f782637cec7020e2
Arya-gupta01 <guptaarya0101@gmail.com>
2026-07-30 10:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (66.51%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,16 +1,28 @@
+// class Solution {
+//     public int minimumPushes(String word) {
+//         int n=word.length();
+//         if(n<=8)return n;
+//         if(n<=16){
+//             return 8+ 2*(n-8);
+//         }
+//         if(n<=24){
+//             return 24+3*(n-16);
+//         }
+        
+//             return 48 +4*(n-24);
+        
+        
+//     }
+// }
+
 class Solution {
     public int minimumPushes(String word) {
-        int n=word.length();
-        if(n<=8)return n;
-        if(n<=16){
-            return 8+ 2*(n-8);
+        int ans = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            ans += i / 8 + 1;
         }
-        if(n<=24){
-            return 24+3*(n-16);
-        }
-        
-            return 48 +4*(n-24);
-        
-        
+
+        return ans;
     }
 }
