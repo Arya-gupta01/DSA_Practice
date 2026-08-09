f183e02857712bfe58849471241a58f1932e2b66
Arya-gupta01 <guptaarya0101@gmail.com>
2026-08-10 00:08:37 +0530

    Time: 2 ms (100.00%) | Memory: 44 MB (88.69%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    int n;
+    int[] suffix;
+    int[][] dp;
+
+    public int stoneGameII(int[] piles) {
+        n = piles.length;
+
+        suffix = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        dp = new int[n][n + 1];
+
+        return solve(0, 1, piles);
+    }
+
+    private int solve(int i, int M, int[] piles) {
+
+        if (i + 2 * M >= n) {
+            return suffix[i];
+        }
+
+        if (dp[i][M] != 0) {
+            return dp[i][M];
+        }
+
+        int ans = 0;
+
+        for (int X = 1; X <= 2 * M; X++) {
+
+            int opponent = solve(
+                i + X,
+                Math.max(M, X),
+                piles
+            );
+
+            int current = suffix[i] - opponent;
+
+            ans = Math.max(ans, current);
+        }
+
+        return dp[i][M] = ans;
+    }
+}
