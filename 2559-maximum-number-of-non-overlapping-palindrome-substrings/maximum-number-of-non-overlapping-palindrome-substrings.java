24e2d32bc8b2697df221c4b88a7e8a363d4106e7
Arya-gupta01 <guptaarya0101@gmail.com>
2026-09-15 23:34:30 +0530

    Time: 2370 ms (5.03%) | Memory: 182.6 MB (9.55%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,31 +1,5 @@
 class Solution {
-    int[][] dp;
-    public int solve(String s, int k , int i , int j){
-        int n=s.length();
-
-
-        if(i>=n || j>=n){
-            return 0;
-        }
-        
-         if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        if(isPal(s,i,j)){
-            int take=1+solve(s,k,j+1,j+k);
-            int grow=solve(s,k,i,j+1);
-            int slide=solve(s,k,i+1,j+1);
-
-            return dp[i][j]=Math.max(take,Math.max(grow,slide));
-        }
-        int grow=solve(s,k,i,j+1);
-            int slide=solve(s,k,i+1,j+1);
-
-            return dp[i][j]=  Math.max(grow,slide);
-        
-    }
-    public boolean isPal(String s , int l ,int r){
+     public boolean isPal(String s , int l ,int r){
 
         while(l<r){
             if(s.charAt(l)!=s.charAt(r)){
@@ -37,19 +11,31 @@ class Solution {
         return true;
     }
     public int maxPalindromes(String s, int k) {
+        int n =s.length();
+
         if(k==1){
-            return s.length();
-        }
-        int n=s.length();
-         dp = new int[n + 1][n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            java.util.Arrays.fill(dp[i], -1);
+            return n ;
         }
 
-        return solve(s,k,0,k-1);
+        int[][] dp=new int[n+1][n+1];
+
+        for(int i= n-1 ; i>=0 ;i--){
+            for(int j =n-1; j>=0 ;j--){
+                if(isPal(s,i,j)){
+                    int take=1+(j+k<=n ? dp[j+1][j+k]:0);
+                    int grow=dp[i][j+1];
+                    int slide=dp[i+1][j+1];
+
+                    dp[i][j]=Math.max(take,Math.max(grow,slide));
+
+                }
+                 int grow=dp[i][j+1];
+                    int slide=dp[i+1][j+1];
+
+                    dp[i][j]=Math.max(dp[i][j],Math.max(grow,slide));
+            }
+        }
+        return dp[0][k-1];
         
     }
-
-    
 }
