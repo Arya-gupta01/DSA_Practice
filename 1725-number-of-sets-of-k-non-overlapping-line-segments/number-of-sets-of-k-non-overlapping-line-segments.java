595cc0e99aadad558ba422f1d9c6f90384d4aa8a
Arya-gupta01 <guptaarya0101@gmail.com>
2026-09-16 23:07:52 +0530

    Time: 1 ms (95.48%) | Memory: 42.6 MB (79.35%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int numberOfSets(int n, int k) {
+        long MOD = 1_000_000_007L;
+
+        int N = n + k - 1;
+        int R = 2 * k;
+
+        long[] fact = new long[N + 1];
+        long[] invFact = new long[N + 1];
+
+        fact[0] = 1;
+
+        for (int i = 1; i <= N; i++) {
+            fact[i] = fact[i - 1] * i % MOD;
+        }
+
+        invFact[N] = power(fact[N], MOD - 2, MOD);
+
+        for (int i = N; i >= 1; i--) {
+            invFact[i - 1] = invFact[i] * i % MOD;
+        }
+
+        return (int) (fact[N] * invFact[R] % MOD
+                    * invFact[N - R] % MOD);
+    }
+
+    private long power(long a, long b, long mod) {
+        long result = 1;
+
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                result = result * a % mod;
+            }
+
+            a = a * a % mod;
+            b >>= 1;
+        }
+
+        return result;
+    }
+}
